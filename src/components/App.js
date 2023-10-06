897014b49b0e0454981bce6729c88b833eb15fd7
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-10-06 18:41:03 +0530

    gender form

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,47 @@ import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  
+  let [gender, setGender] = useState("") 
+  console.log(gender)
   return (
     <div>
         <h1>Select your gender</h1>
-        <input type="radio" defaultChecked/>
+        <input type="radio" 
+        name="gender"
+        value="Male" onChange={(e) => setGender(e.target.value)} />
         <label>Male</label>
-        <input type="radio" />
+
+        <input 
+        type="radio"
+        name="gender"
+         value="Female" onChange={(e) => setGender(e.target.value)}/>
         <label>Female</label>
+        
+        {
+          gender == "Male" &&
+          <div>
+            <h2>Select your shirt size</h2>
+            <select>
+              <option>Select your size:</option>
+              <option>Small</option>
+              <option>Medium</option>
+              <option>Large</option>
+            </select>
+          </div>
+        }
+
+{
+          gender == "Female" &&
+          <div>
+            <h2>Select your dress size</h2>
+            <select>
+              <option>Select size:</option>
+              <option>2</option>
+              <option>4</option>
+              <option>6</option>
+            </select>
+          </div>
+        }
 
     </div>
   )
